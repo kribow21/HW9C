068df52633e8e410dcaf3c138726bebc28433e3b
kribow21 <kristininnotech@gmail.com>
2021-07-09 18:26:31 -0600

    created an array of tweets and made a loop to log each of them.

---
+++ basicbash/js/app.js
@@ -0,0 +1,17 @@
+//created array for tweets, each line is a single tweet;
+var tweets = ["My favorite colour is purple.",
+"Going on a trip to Calgary, see you in 3 days!",
+"I got a puppy today it's name is Luna, she's a corgi.",
+"Wow, school is tough right now but im trying to power through it. All the support is appreciated.",
+"Looking forward to roadtripping and visiting my best friend in Whitecourt AB.",
+"Yoga class is the only thing keeping me from not going crazy.",
+"Can't wait to see my favourite band in September!",
+"Looking forward to the day I can book a vacation and be on an airplane!",
+"People with air conditioning don't appreciate it enough! What i would do for one of those...", 
+"I Can't beleive its already July, this summer is flying by."];
+var counter = 0;
+//set up while loop;
+while (counter <=9){
+    console.log(tweets[counter]);
+    counter = counter +1;
+}
